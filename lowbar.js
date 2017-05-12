31e230dcff1192c7dea84bd6af9cc596ffa42ec1
dentednerd <jo.imlay@gmail.com>
2017-05-12 12:10:47 +0100

    [_.each] modifies each item in a list

--- lowbar.js
+++ lowbar.js
@@ -1,8 +1,49 @@
 var _ = {};
-
-_.identity = function() {
-  // your code here 
+// identity
+_.identity = function(x) {
+   return x;
 };
+
+// first
+_.first = function(array,num) {
+  if (num === undefined) {
+  return array[0];
+}
+else {
+  return array.slice(0,num);
+}
+
+};
+
+// last
+_.last = function(array,num) {
+  if (num === undefined) {
+    return array[array.length - 1];
+  }
+  if (array.length < num) {
+    return array;
+  }
+  else {
+    return array.slice(array.length - num,array.length);
+  }
+
+};
+
+// each
+_.each = function(list, func) {
+  if (list[0]) {
+    var result = list.map(func);
+    return result;
+  }
+  else {
+    var res = [];
+    for (var key in list) {
+      res.push(func(list[key]));
+    }
+    return res;
+  }
+};
+
 
 if (typeof module !== 'undefined') {
   module.exports = _;
---
+++ spec/lowbar.spec.js
@@ -0,0 +1,105 @@
+/* global describe, it */
+var path = require('path');
+var expect = require('chai').expect;
+
+var _ = require(path.join(__dirname, '..', './lowbar.js'));
+
+describe('_', function() {
+  'use strict';
+
+  it('is an object', function() {
+    expect(_).to.be.an('object');
+  });
+  // identify
+  describe('#identity', function() {
+    it('is a function', function() {
+      expect(_.identity).to.be.a('function');
+    });
+    it('should return the same data type', function() {
+      var result = _.identity([1, 2, 3]);
+      expect(Array.isArray(result)).to.equal(true);
+      var nums = _.identity(4567);
+      expect(Number.isInteger(nums)).to.equal(true);
+      var str = _.identity('hello world!');
+      expect(typeof str).to.be.a('string');
+      var undef = _.identity(undefined);
+      expect(undef).to.be.a('undefined');
+      var boolean = _.identity(true);
+      expect(boolean).to.be.a('boolean');
+      var nul = _.identity(null);
+      expect(nul).to.be.a('null');
+    });
+    it('should return the given value', function() {
+      var result = _.identity(5);
+      expect(result).to.equal(5);
+    });
+    // first
+    describe('#first', function() {
+      it('is a function', function() {
+        expect(_.first).to.be.a('function');
+      });
+      it('should return the first element in an array if no n parameter present', function() {
+        var result = _.first([1, 2, 3]);
+        expect(result).to.equal(1);
+      });
+      it('should return the first  n elements in an array if n parameter present', function() {
+        var result = _.first([1, 2, 3],2);
+        expect(result).to.eql([1,2]);
+      });
+      it('should return an array if n parameter is present', function() {
+        var result = _.first([4, 5, 6],2);
+        expect(result).to.eql([4,5]);
+      });
+      it('should return the array if n is greater than the length of the array',function() {
+        var result = _.first([1, 2, 3, 4],7);
+        expect(result).to.eql([1, 2, 3, 4]);
+      });
+      it('should receive an array', function() {
+        var result = _.first([1, 2, 3, 4]);
+        expect(result).to.equal(1);
+      });
+     });
+     // last
+     describe('#last', function() {
+      it('is a function', function() {
+        expect(_.last).to.be.a('function');
+      });
+      it('should return the last element in an array if no n parameter present', function() {
+        var result = _.last([1, 2, 3]);
+        expect(result).to.equal(3);
+      });
+      it('should return the last  n elements in an array if n parameter present', function() {
+        var result = _.last([1, 2, 3],2);
+        expect(result).to.eql([2,3]);
+      });
+      it('should return an array if n parameter is present', function() {
+        var result = _.last([4, 5, 6],2);
+        expect(result).to.eql([5,6]);
+      });
+      it('should return the array if n is greater than the length of the array',function() {
+        var result = _.last([1, 2, 3, 4],7);
+        expect(result).to.eql([1, 2, 3, 4]);
+      });
+      it('should receive an array', function() {
+        var result = _.last([1, 2, 3, 4]);
+        expect(result).to.equal(4);
+      });
+    });
+    // each
+    describe('#each', function() {
+      it('is a function', function() {
+        expect(_.each).to.be.a('function');
+      });
+      it('should return an array of the same length', function() {
+        var result = _.each([1,2,3,4], function(n) {return n * 2;});
+        expect(result.length).to.equal(4);
+      });
+      it('should handle both objects and arrays', function() {
+        var result = _.each([1,2,3,4], function(n) {return n * 2;});
+        expect(result).to.eql([2,4,6,8]);
+        var result2 = _.each({a:1, b:2, c:3}, function(n) {return n * 2;});
+        expect(result2).to.eql([2,4,6]);
+      });
+});
+});
+});
