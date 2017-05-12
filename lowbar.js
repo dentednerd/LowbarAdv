9f98b48955bd0fccc81f643301cde64e94fd59cf
dentednerd <jo.imlay@gmail.com>
2017-05-12 14:51:24 +0100

    [_.reject] returns a list filtered by the opposite of the predicate

--- lowbar.js
+++ lowbar.js
@@ -53,6 +53,15 @@ _.filter = function(list, predicate) {
   } return result;
 };
 
+_.reject = function(list, predicate) {
+  var result = [];
+  for (var key in list) {
+    if (!predicate(list[key])) {
+      result.push(list[key]);
+    }
+  } return result;
+};
+
 if (typeof module !== 'undefined') {
   module.exports = _;
 }
