6939b4ecd0ec492170d0fb46cd4724f85cedc8ea
Jo Imlay <jo.imlay@gmail.com>
2017-08-07 15:23:08 +0100

    [_.first] returns the first n elements of an array

--- lowbar.js
+++ lowbar.js
@@ -5,6 +5,15 @@ _.identity = function(x) {
    return x;
 };
 
+// first
+_.first = function(array,num) {
+  if (num === undefined) {
+    return array[0];
+  } else {
+    return array.slice(0,num);
+  }
+};
+
 
 if (typeof module !== 'undefined') {
   module.exports = _;
